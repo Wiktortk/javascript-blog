260948c6fa05d49d08dd4a4f1238545f5240d5fa
Wiktor Kulikowski <scotti.pol@gmail.com>
2021-04-24 17:45:39 +0200

    execution of tasks: [DONE] find the correct article using the selector (value of 'href' attribute) and [DONE] add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -21,12 +21,11 @@ const titleClickHandler = function(event){
   /* [DONE] add class 'active' to the clicked link */
 
   clickedElement.classList.add('active');
-  //console.log('clickedElement (with plus): ' + clickedElement); PRZEZ ZAPIS Z '+' WYNIK NA CONSOLI PODAJE MNIEJ INF NIZ ZAPIS BEZ '+'
   console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.posts article.active'); 
+  const activeArticles = document.querySelectorAll('.post.active'); 
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
@@ -37,13 +36,15 @@ const titleClickHandler = function(event){
   const articleSelector = clickedElement.getAttribute('href');
   console.log('articleSelector:', articleSelector);
 
-  /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-  const targetArticle = document.querySelector('.posts #');
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle); //zły zapis - nie działa 
+  console.log('targetArticle:',targetArticle);
 
-  console.log('targetArticle:', targetArticle);
+  /* [DONE] add class 'active' to the correct article */
 
-  /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');  
 }
 
 const links = document.querySelectorAll('.titles a');
